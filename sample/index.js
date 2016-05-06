d47b6c3a6e84432d3371608148f9784ce1462891
Phillip M. Hoff <phillip@orst.edu>
2016-05-06 16:28:54 -0700

    Added basic login() function.

--- sample/index.js
+++ sample/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const carwings = require('../release');
+const secrets = require('./secrets.json');
 
-carwings.Client.connect(err => {
+carwings.Client.login(secrets.email, secrets.password, err => {
     if (err) {
         console.error(err);
         
--- src/Client.ts
+++ src/Client.ts
@@ -1,12 +1,15 @@
+import * as crypto from 'crypto';
 import * as request from 'request';
 
 export class Client {
     private static BASE_ENDPOINT = 'https://gdcportalgw.its-mo.com/gworchest_0307C/gdc';
 
-    public static connect(callback: (err?: Error) => void) {
-        request({
+    /* tslint:disable no-any */
+    public static connect(callback: (err?: Error, response?: any) => void) {
+    /* tslint:enable no-any */
+        request.post({
             url: Client.BASE_ENDPOINT + '/InitialApp.php',
-            formData: {
+            form: {
                 'RegionCode': 'NNA',
                 'lg': 'en-US',
                 'initial_app_strings': 'geORNtsZe5I4lRGjG9GZiA',
@@ -14,7 +17,41 @@ export class Client {
             }
         },
         (err, response, body) => {
-            callback(err);
+            if (err) {
+                return callback(err);
+            }
+
+            const parsedResponse = JSON.parse(body);
+
+            callback(undefined, parsedResponse);
         });
+    }
+
+    public static login(userId: string, password: string, callback: (err?: Error) => void) {
+        Client.connect(
+            (connectErr, connectResponse) => {
+                const cipher = crypto.createCipheriv('bf-ecb', new Buffer(connectResponse.baseprm), new Buffer(''));
+
+                let encrypted = cipher.update(password, 'utf8', 'base64');
+                encrypted += cipher.final('base64');
+
+                request.post({
+                    url: Client.BASE_ENDPOINT + '/UserLoginRequest.php',
+                    form: {
+                        'RegionCode': 'NNA',
+                        'lg': 'en-US',
+                        'UserId': userId,
+                        'Password': encrypted,
+                        'initial_app_strings': 'geORNtsZe5I4lRGjG9GZiA'
+                    }
+                },
+                (err, response, body) => {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(undefined);
+                });
+            });
     }
 }
