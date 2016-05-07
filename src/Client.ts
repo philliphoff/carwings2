c9be761fd075d8853e00a1c26be01a402266ec9d
Phillip M. Hoff <phillip@orst.edu>
2016-05-06 17:42:02 -0700

    Extract and store custom session ID.

--- src/Client.ts
+++ src/Client.ts
@@ -5,6 +5,7 @@ export class Client {
     private static BASE_ENDPOINT = 'https://gdcportalgw.its-mo.com/gworchest_0307C/gdc';
     private static INITIAL_APP_STRINGS = 'geORNtsZe5I4lRGjG9GZiA';
 
+    private _customSessionId: string;
     private _regionCode: string;
     private _locale: string;
 
@@ -19,8 +20,7 @@ export class Client {
             form: {
                 'RegionCode': this._regionCode,
                 'lg': this._locale,
-                'initial_app_strings': Client.INITIAL_APP_STRINGS,
-                'custom_sessionid': ''
+                'initial_app_strings': Client.INITIAL_APP_STRINGS
             }
         },
         (err, response, body) => {
@@ -75,6 +75,8 @@ export class Client {
 
                     const parsedBody = JSON.parse(body);
 
+                    that._customSessionId = Client.extractCustomSessionIdFromLoginResponse(parsedBody);
+
                     callback(undefined, parsedBody);
                 });
             });
@@ -88,5 +90,45 @@ export class Client {
         encrypted += cipher.final('base64');
 
         return encrypted;
+    }
+
+    private static extractCustomSessionIdFromLoginResponse(response): string {
+        const vehicleInfoList = response.VehicleInfoList;
+
+        if (!vehicleInfoList) {
+            console.warn('Response did not include a vehicle information list.');
+            return;
+        };
+
+        const vehicleInfo = vehicleInfoList.vehicleInfo;
+
+        if (!vehicleInfo) {
+            console.warn('Response did not include vehicle information.');
+            return;
+        }
+
+        if (!(vehicleInfo instanceof Array)) {
+            console.warn('Vehicle information property is not an array.');
+            return;
+        }
+
+        if (vehicleInfo.length === 0) {
+            console.warn('Response did not include ag vehicle.');
+            return;
+        }
+
+        if (vehicleInfo.length > 1) {
+            console.warn('Response included more than one vehicle.');
+        }
+
+        const vehicle = vehicleInfo[0];
+
+        const customSessionId = vehicle.custom_sessionid;
+
+        if (!customSessionId) {
+            console.warn('Response did not include a custom session ID.');
+        }
+
+        return customSessionId;
     }
 }
