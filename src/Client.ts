f65ca8e04955d009524dc32e456248cdf93db953
Phillip M. Hoff <phillip@orst.edu>
2016-05-06 17:13:44 -0700

    Client is now stateful, returns vehicle data.

--- src/Client.ts
+++ src/Client.ts
@@ -3,16 +3,23 @@ import * as request from 'request';
 
 export class Client {
     private static BASE_ENDPOINT = 'https://gdcportalgw.its-mo.com/gworchest_0307C/gdc';
+    private static INITIAL_APP_STRINGS = 'geORNtsZe5I4lRGjG9GZiA';
 
-    /* tslint:disable no-any */
-    public static connect(callback: (err?: Error, response?: any) => void) {
-    /* tslint:enable no-any */
+    private _regionCode: string;
+    private _locale: string;
+
+    public constructor(regionCode?: string, locale?: string) {
+        this._regionCode = regionCode || 'NNA'; // Default to North America
+        this._locale = locale || 'en-US';       // Default to English (US)
+    }
+
+    public connect(callback: (err?: Error, passwordEncryptionKey?: string) => void): void {
         request.post({
             url: Client.BASE_ENDPOINT + '/InitialApp.php',
             form: {
-                'RegionCode': 'NNA',
-                'lg': 'en-US',
-                'initial_app_strings': 'geORNtsZe5I4lRGjG9GZiA',
+                'RegionCode': this._regionCode,
+                'lg': this._locale,
+                'initial_app_strings': Client.INITIAL_APP_STRINGS,
                 'custom_sessionid': ''
             }
         },
@@ -21,28 +28,40 @@ export class Client {
                 return callback(err);
             }
 
-            const parsedResponse = JSON.parse(body);
+            if (response.statusCode !== 200) {
+                return callback(new Error('Response was status code: ' + response.statusCode + ' (' + response.statusMessage + ')'));
+            }
 
-            callback(undefined, parsedResponse);
+            const parsedBody = JSON.parse(body);
+            const passwordEncryptionKey = parsedBody.baseprm;
+
+            if (!passwordEncryptionKey) {
+                return callback(new Error('Response did not include password encryption key.'));
+            }
+
+            return callback(undefined, passwordEncryptionKey);
         });
     }
 
-    public static login(userId: string, password: string, callback: (err?: Error) => void) {
-        Client.connect(
-            (connectErr, connectResponse) => {
-                const cipher = crypto.createCipheriv('bf-ecb', new Buffer(connectResponse.baseprm), new Buffer(''));
+    public login(userId: string, password: string, callback: (err?: Error, vehicle?: Object) => void): void {
+        const that = this;
 
-                let encrypted = cipher.update(password, 'utf8', 'base64');
-                encrypted += cipher.final('base64');
+        this.connect(
+            (connectErr, passwordEncryptionKey) => {
+                if (connectErr) {
+                    return callback(connectErr);
+                }
+
+                const encryptedPassword = Client.encryptPassword(password, passwordEncryptionKey);
 
                 request.post({
                     url: Client.BASE_ENDPOINT + '/UserLoginRequest.php',
                     form: {
-                        'RegionCode': 'NNA',
-                        'lg': 'en-US',
+                        'RegionCode': that._regionCode,
+                        'lg': that._locale,
                         'UserId': userId,
-                        'Password': encrypted,
-                        'initial_app_strings': 'geORNtsZe5I4lRGjG9GZiA'
+                        'Password': encryptedPassword,
+                        'initial_app_strings': Client.INITIAL_APP_STRINGS
                     }
                 },
                 (err, response, body) => {
@@ -50,8 +69,24 @@ export class Client {
                         return callback(err);
                     }
 
-                    callback(undefined);
+                    if (response.statusCode !== 200) {
+                        return callback(new Error('Response was status code: ' + response.statusCode + ' (' + response.statusMessage + ')'));
+                    }
+
+                    const parsedBody = JSON.parse(body);
+
+                    callback(undefined, parsedBody);
                 });
             });
+    }
+
+    private static encryptPassword(password: string, passwordEncryptionKey: string) {
+        const cipher = crypto.createCipheriv('bf-ecb', new Buffer(passwordEncryptionKey), new Buffer(''));
+
+        let encrypted = cipher.update(password, 'utf8', 'base64');
+
+        encrypted += cipher.final('base64');
+
+        return encrypted;
     }
 }
