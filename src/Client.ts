02e1567beab72e3fc793d792e01c11c8acf0abf7
Phillip M. Hoff <phillip@orst.edu>
2016-05-12 12:12:27 -0700

    Extract session-related data from login response.

--- src/Client.ts
+++ src/Client.ts
@@ -1,3 +1,5 @@
+import { IVehicle } from './IVehicle';
+
 import * as crypto from 'crypto';
 import * as request from 'request';
 
@@ -6,8 +8,11 @@ export class Client {
     private static INITIAL_APP_STRINGS = 'geORNtsZe5I4lRGjG9GZiA';
 
     private _customSessionId: string;
-    private _regionCode: string;
+    private _dcmId: string;
+    private _gdcUserId: string;
     private _locale: string;
+    private _regionCode: string;
+    private _timeZone: string;
 
     public constructor(regionCode?: string, locale?: string) {
         this._regionCode = regionCode || 'NNA'; // Default to North America
@@ -43,7 +48,7 @@ export class Client {
         });
     }
 
-    public login(userId: string, password: string, callback: (err?: Error, vehicle?: Object) => void): void {
+    public login(userId: string, password: string, callback: (err?: Error, vehicle?: IVehicle) => void): void {
         const that = this;
 
         this.connect(
@@ -77,9 +82,26 @@ export class Client {
 
                     that._customSessionId = Client.extractCustomSessionIdFromLoginResponse(parsedBody);
 
-                    callback(undefined, parsedBody);
+                    const customerInfo = Client.extractCustomerInfo(parsedBody);
+
+                    that._timeZone = customerInfo.timeZone;
+
+                    const vehicleInfo = Client.extractVehicleInfo(parsedBody);
+
+                    that._dcmId = vehicleInfo.dcmId;
+                    that._gdcUserId = vehicleInfo.gdcUserId;
+
+                    callback(
+                        undefined,
+                        {
+                            vin: vehicleInfo.vin
+                       });
                 });
             });
+    }
+
+    public requestStatus(vin: string, callback: (err?: Error, status?: Object) => void): void {
+        // No-op.
     }
 
     private static encryptPassword(password: string, passwordEncryptionKey: string) {
@@ -113,7 +135,7 @@ export class Client {
         }
 
         if (vehicleInfo.length === 0) {
-            console.warn('Response did not include ag vehicle.');
+            console.warn('Response did not include a vehicle.');
             return;
         }
 
@@ -130,5 +152,73 @@ export class Client {
         }
 
         return customSessionId;
+    }
+
+    private static extractCustomerInfo(response): {
+        timeZone: string
+    }{
+        const customerInfo = response.CustomerInfo;
+
+        if (!customerInfo) {
+            console.warn('Response did not include a customer info object.');
+
+            return;
+        }
+
+        const timeZone = customerInfo.Timezone;
+
+        if (!timeZone) {
+            console.warn('Response did not include a timezone.');
+        }
+
+        return {
+            timeZone: timeZone
+        };
+    }
+
+    private static extractVehicleInfo(response): {
+        gdcUserId: string,
+        dcmId: string,
+        vin: string
+    }{
+        const vehicle = response.vehicle;
+
+        if (!vehicle) {
+            console.warn('Response did not include a vehicle object.');
+
+            return;
+        }
+
+        const profile = vehicle.profile;
+
+        if (!profile) {
+            console.warn('Response did not include a profile.');
+
+            return;
+        }
+
+        const gdcUserId = profile.gdcUserId;
+
+        if (!gdcUserId) {
+            console.warn('Response did not include a GDC user ID.');
+        }
+
+        const dcmId = profile.dcmId;
+
+        if (!dcmId) {
+            console.warn('Response did not include a DCM ID.');
+        }
+
+        const vin = profile.vin;
+
+        if (!vin) {
+            console.warn('Response did not include a VIN.');
+        }
+
+        return {
+            gdcUserId: gdcUserId,
+            dcmId: dcmId,
+            vin: vin
+        }
     }
 }
