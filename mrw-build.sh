9109247b035c7dd84303b8433316e2a0b6150ae5
Matthew Whitehead <matthew1001@gmail.com>
2018-10-26 16:38:10 +0000

    Change my carwings2 build script so that it assumes a sensible version of node is installed, rather than messing around trying to run nvm and automatically install a good version

--- mrw-build.sh
+++ mrw-build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [ -f ~/.nvm/nvm.sh ]; then
-  . ~/.nvm/nvm.sh install lts/boron
-  . ~/.nvm/nvm.sh use lts/boron
-  npm install .
-  sudo npm install gulp-cli -g
-  sudo npm install gulp -D
-  sudo npm install npx -g
-  npm install babel-register
-  npx -p touch nodetouch gulpfile.js
-  gulp build -f gulpfile.babel.js
-  sudo npm link
-else
-  echo "Install NVM so the script can install the correct version of nodejs"
-fi
+npm install .
+sudo npm install gulp-cli -g
+sudo npm install gulp -D
+sudo npm install npx -g
+npm install babel-register
+npx -p touch nodetouch gulpfile.js
+gulp build -f gulpfile.babel.js
+sudo npm link
 
